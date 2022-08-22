096b25071cf4c786173e07fa19d91fa6736f8cac
Antonio Huerta Reyes <anhr9728@gmail.com>
2022-08-21 20:10:16 -0500

    Añade la sección de galeria en nosotros

--- src/app/components/nosotrxs/nosotrxs-images/nosotrxs-images.component.ts
+++ src/app/components/nosotrxs/nosotrxs-images/nosotrxs-images.component.ts
@@ -10,6 +10,17 @@ export class NosotrxsImagesComponent implements OnInit {
 
     @Input() size!: Number;
 
+    minimalGallery: Array<string> = [
+    "https://picsum.photos/200/300?random=1",
+    "https://picsum.photos/200/300?random=2",
+    "https://picsum.photos/200/300?random=3",
+    "https://picsum.photos/200/300?random=4",
+    "https://picsum.photos/200/300?random=5",
+    "https://picsum.photos/200/300?random=6",
+    "https://picsum.photos/200/300?random=7",
+    "https://picsum.photos/200/300?random=8",
+  ]
+
     constructor(private googleDriveService: GoogleDriveService) {
     }
 
