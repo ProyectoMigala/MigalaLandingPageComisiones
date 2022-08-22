fefdbe5d47e2c78933097797f5616b3d4f3454dc
Joshua Saucedo <joshuansu0897@gmail.com>
2022-08-21 17:07:24 -0700

    social media dynamic

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../environments/environment.prod';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+  estado = environment.estado;
+  socialMedia = environment.socialMedia;
 
   constructor() { }
 
